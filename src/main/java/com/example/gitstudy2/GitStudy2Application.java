3d85d367c97b3bd00a121564639c7860ad10a0ba
dkfdj <dkfdj01033@gmail.com>
2025-01-10 01:30:11 +0900

    add new Line

--- src/main/java/com/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudy2Application {
 
     public static void main(String[] args) {
+        System.out.println("newline");
         SpringApplication.run(GitStudy2Application.class, args);
-    }
 
+    }
 }

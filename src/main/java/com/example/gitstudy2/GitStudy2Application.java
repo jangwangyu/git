f0e6f67a02e1bc04126cb3f1f49aa3dd4db5c2f0
dkfdj <dkfdj01033@gmail.com>
2025-01-10 15:26:31 +0900

    test2

--- src/main/java/com/example/gitstudy2/GitStudy2Application.java
+++ src/main/java/com/example/gitstudy2/GitStudy2Application.java
@@ -11,6 +11,7 @@ public class GitStudy2Application {
         System.out.println("a");
         System.out.println("b");
         System.out.println("c");
+        System.out.println("d");
         SpringApplication.run(GitStudy2Application.class, args);
 
     }
